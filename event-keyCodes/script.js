6932c2dd614a606b6760b0a4baf7c52760416a2c
unknown <volkan.goksu@outlook.com>
2021-07-30 13:53:48 +0300

    window keydown eventlistener

---
+++ event-keyCodes/script.js
@@ -0,0 +1,20 @@
+var insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+  // console.log(e);
+  insert.innerHTML = `
+  <div class="key">
+  ${e.key === ' ' ? 'Space' : e.key}
+  <small>e.key</small>
+</div>
+<div class="key">
+  ${e.keyCode}
+  <small>e.keyCode</small>
+</div>
+<div class="key">
+   ${e.code}
+  <small>e.code</small>
+</div>
+
+  `
+})
